5b3c868260aad5fea5672e3c4e5bb3ec6d89e615
alsandre <gabeditebuli@gmail.com>
2024-09-24 22:12:00 +0400

    working but slow and inefficient approach

--- 24-09-2024__24-10-2024/24-09/3043.ts
+++ 24-09-2024__24-10-2024/24-09/3043.ts
@@ -1,23 +1,43 @@
-// BRUTE APPROACH
+// BRUTE APPROACH - OUT of TIME
+// function longestCommonPrefix(arr1: number[], arr2: number[]): number {
+//   let res = 0;
+//   arr1.forEach((num1) => {
+//     arr2.forEach((num2) => {
+//       let prfxCounter = 0;
+//       let num1Str = num1.toString();
+//       let num2Str = num2.toString();
+//       let pointer = 0;
+//       while (
+//         num1Str[pointer] === num2Str[pointer] &&
+//         (num1Str.length >= pointer || num2Str.length >= pointer)
+//       ) {
+//         prfxCounter++;
+//         pointer++;
+//       }
+//       if (prfxCounter > res) res = prfxCounter;
+//     });
+//   });
+
+//   return res;
+// }
+
+// BRUTE but WORKING APPROACH
 function longestCommonPrefix(arr1: number[], arr2: number[]): number {
+  let hash1: {[key:string]: boolean} = {};
   let res = 0;
   arr1.forEach((num1) => {
-    arr2.forEach((num2) => {
-      let prfxCounter = 0;
-      let num1Str = num1.toString();
-      let num2Str = num2.toString();
-      let pointer = 0;
-      while (
-        num1Str[pointer] === num2Str[pointer] &&
-        (num1Str.length >= pointer || num2Str.length >= pointer)
-      ) {
-        prfxCounter++;
-        pointer++;
-      }
-      if (prfxCounter > res) res = prfxCounter;
-    });
+    let num1Str = num1.toString();
+    for (let i = num1Str.length; i >= 0; i--) {
+      hash1[num1Str.slice(0, i)] = true;
+    }
+  });
+  arr2.forEach((num2) => {
+    let num2Str = num2.toString();
+    for (let i = num2Str.length; i >= 0; i--) {
+        let num2Pref = num2Str.slice(0, i);
+      if(hash1[num2Pref] && num2Pref.length > res) res = num2Pref.length
+    }
   });
 
   return res;
 }
-// OUT of TIME ...
