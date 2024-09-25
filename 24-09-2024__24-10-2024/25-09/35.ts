735f5c8cac0a0fa443a587a05e8005950bdfe7e2
alsandre <gabeditebuli@gmail.com>
2024-09-25 11:20:04 +0400

    algo implementation

--- 24-09-2024__24-10-2024/25-09/35.ts
+++ 24-09-2024__24-10-2024/25-09/35.ts
@@ -1,6 +1,6 @@
 // Overview:
 // one of the constraints says to implement algorithm with complexity of O(log n)
-// that means that we have to go over given array at most once 
+// that means that we have to go over given array at most once
 // this would be when there is no target and we have to check every single element
 // in most cases we should finish premature whenever we find the answer
 // it is possible since array is already sorted, if else it would be impossible since there is no such algo that would sort array in or under given time complexity
@@ -12,3 +12,16 @@
 // step3: if matched comparision, return current index
 // step3: if item is greater then target return current index
 // step4: if non condition satisfied, return length of an array as an result
+
+function searchInsert(nums: number[], target: number): number {
+  let res = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    let item = nums[i];
+    if (item === target || item > target) {
+      res = i;
+      break;
+    }
+  }
+
+  return res;
+}
