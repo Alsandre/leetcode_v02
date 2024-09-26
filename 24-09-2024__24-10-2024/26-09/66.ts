4d1e833cb8a9da05c8b12d6b552bb3943afd7667
alsandre <gabeditebuli@gmail.com>
2024-09-26 11:23:11 +0400

    implementation

--- 24-09-2024__24-10-2024/26-09/66.ts
+++ 24-09-2024__24-10-2024/26-09/66.ts
@@ -12,8 +12,23 @@
 
 // we have a constraint that array length is between 1 and 100 so we dont need to validate empty array scenario (s1)
 
-// Algo: 
+// Algo:
 // step 1 - setup backward loop
 // step 2 - if value is less then 9 replace it with incremented value and break
 // step 3 - if value is 9 and index is not 0 set it to 0
 // step 4 - if value is 9 and index is 0 set value to 0 and unshift 1 to the array
+
+function plusOne(digits: number[]): number[] {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      ++digits[i];
+      break;
+    } else if (digits[i] === 9 && i > 0) {
+      digits[i] = 0;
+    } else {
+      digits[i] = 0;
+      digits.unshift(1);
+    }
+  }
+  return digits;
+}
