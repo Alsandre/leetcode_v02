1863ff181e7724855cd922d59e91153e27dcb964
alsandre <gabeditebuli@gmail.com>
2024-09-29 17:05:54 +0400

    setup

---
+++ 24-09-2024__24-10-2024/29-09/118.ts
@@ -0,0 +1,6 @@
+// Pascals again this time we are working with whole triangle up to index instead of just last row.
+// I think that for this situation, using first brute approach would be more straightforward
+
+function generate(numRows: number): number[][] {
+    
+};
