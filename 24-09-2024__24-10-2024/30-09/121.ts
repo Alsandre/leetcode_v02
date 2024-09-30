f1c9166431dd93174b655b5685590e336e9b99c9
alsandre <gabeditebuli@gmail.com>
2024-09-30 10:36:06 +0400

    setup

---
+++ 24-09-2024__24-10-2024/30-09/121.ts
@@ -0,0 +1,13 @@
+// The problem challanges us to find a pair in ascending order
+// that has the highest difference
+// we should consider all valid pair
+// and choose most appropriate of them
+
+// algo:
+
+// step 1 - create variable diff and set it to 0
+// step 2 - for each element in a list
+// step 3 - find difference of element to all remaining elements
+// step 4 - if result is higher than diff, reassign difference to diff
+// step 5 - return diff
+
