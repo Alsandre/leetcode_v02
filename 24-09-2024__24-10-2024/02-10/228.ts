6f83c5c1fff8e8e6b86809f73b5adb57ccf3c085
alsandre <gabeditebuli@gmail.com>
2024-10-02 09:01:30 +0400

    implemented algo, but does not work

--- 24-09-2024__24-10-2024/02-10/228.ts
+++ 24-09-2024__24-10-2024/02-10/228.ts
@@ -3,10 +3,30 @@
 
 // we should return array of ranges
 
-// algo: 
+// algo:
 // step 1 - create empty array "ranges"
-// step 2 - iterate over given array
-// step 3 - create temporary variable rangeStart
+// step 2 - create temporary variable rangeStart, set it to first element
+// step 3 - iterate over given array
 // step 4 - if next element in array is greater by 1, continue
 // step 5 - if difference with next element is greater than 1, close range, push range to ranges, clear rangeStart
 // step 6 - return ranges
+
+function summaryRanges(nums: number[]): string[] {
+  let ranges: string[] = [];
+  let rangeStart = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const diff = nums[i] - rangeStart;
+    if (diff === 1) continue;
+    if (diff > 1) {
+      const range =
+        nums[i - 1] === rangeStart
+          ? `${rangeStart}`
+          : `${rangeStart}->${nums[i - 1]}`;
+      ranges.push(range);
+      rangeStart = nums[i];
+    }
+  }
+  return ranges;
+}
+
+console.log(summaryRanges([0,2,3,4,6,8,9]));
