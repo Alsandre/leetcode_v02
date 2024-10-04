ab3941508851acd5e43d4e2cf2e8250c168e0381
alsandre <gabeditebuli@gmail.com>
2024-10-04 10:32:53 +0400

    overview and algo

---
+++ 24-09-2024__24-10-2024/04-10/303.ts
@@ -0,0 +1,8 @@
+// so design problem, sounds interesting
+// seems trivial, why would we mutate array for calculating inclusive sum in range?
+
+// sum algo:
+// step 1 - create variable acc
+// step 2 - create loop from left to right inclusive
+// step 2 - for each iteration access element at given position in nums and add to acc
+// step 3 - return acc
