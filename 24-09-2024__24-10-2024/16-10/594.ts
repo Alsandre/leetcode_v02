e950637b3f091401de97c89173e6218d6e6b108b
alsandre <gabeditebuli@gmail.com>
2024-10-16 13:02:33 +0400

    გადაჭრა

--- 24-09-2024__24-10-2024/16-10/594.ts
+++ 24-09-2024__24-10-2024/16-10/594.ts
@@ -4,3 +4,18 @@
 // შევქმნათ შემნახველი ობიექტი რომმლის გასაღებიც იქნება ელემენტი
 // ხოლო მნიშვნელობა ამ ელემენტის სიხშირე
 
+function findLHS(nums: number[]): number {
+  let max: number = 0;
+  let map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+  }
+  for (let [key, length] of map) {
+    let prev = map.get(key - 1);
+    if (!prev) continue;
+    console.log(map.get(key - 1));
+    max = Math.max(max, prev! + length);
+  }
+  return max;
+}
+findLHS([1, 3, 2, 2, 5, 2, 3, 7]);
