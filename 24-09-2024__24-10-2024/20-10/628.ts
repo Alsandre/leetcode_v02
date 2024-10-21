c58f98a0210735600f3ab932f287c0d620176136
alsandre <gabeditebuli@gmail.com>
2024-10-21 11:18:00 +0400

    შესწორებები

--- 24-09-2024__24-10-2024/20-10/628.ts
+++ 24-09-2024__24-10-2024/20-10/628.ts
@@ -23,34 +23,40 @@
 // ნაბიჯი 3 - თუ უდიდესი ელემენტი უარყოფითია ან უმცირესი ელემენტი დადებითია, მაშინ დავაბრუნოთ უდიდესი სამი ელემენტის ნამრავლი
 // ნაბიჯი 4 - თუ ორი უმცირესი მნიშვნელობის ნამრავლი მეტია მე-2 და მე-3 უდიდესი მნიშვნელობების ნამრავლზე, მაშინ დავაბრუნოთ ორი უმცირესი და ერთი უდიდეს მნიშვნელობათა ნამრავლი
 
-function maximumProduct(nums: number[]): number {
-    let sorted = mergeSort(nums);
-    let len = sorted.length
-    let min1 = sorted[0];
-    let min2 = sorted[1];
-    let min3 = sorted[3];
-    let max1 = sorted[len-1]
-    let max2 = sorted[len-2]
-    let max3 = sorted[len-3]
-    
-    if(max1 < 0 || min1 > 0) {
-      return max1 * max2 * max3
-    }
-    if(min1 * min2 >  max2 * max3) {
-      return min1 * min2 * max1
-    }
+function _maximumProduct(nums: number[]): number {
+  let sorted = mergeSort(nums);
+  let len = sorted.length;
+  let min1 = sorted[0];
+  let min2 = sorted[1];
+  let min3 = sorted[2];
+  let max1 = sorted[len - 1];
+  let max2 = sorted[len - 2];
+  let max3 = sorted[len - 3];
 
+  if (min1 > 0) {
+    console.log("max1>");
+    return max1 * max2 * max3;
+  }
+  if (max1 < 0) {
+    console.log("max1<");
+    return min1 * min2 * min3;
+  }
+  if (min1 * min2 > max2 * max3) {
+    console.log("min max");
+    return min1 * min2 * max1;
+  }
+  return 0;
 }
 
 function mergeSort(numArray: number[]): number[] {
   if (numArray.length <= 1) {
     return numArray;
   }
-  const mid = Math.floor(numArray.length/2)
-  const left = numArray.slice(0, mid)
+  const mid = Math.floor(numArray.length / 2);
+  const left = numArray.slice(0, mid);
   const right = numArray.slice(mid, numArray.length);
-  const leftSorted = mergeSort(left)
-  const rightSorted = mergeSort(right)
+  const leftSorted = mergeSort(left);
+  const rightSorted = mergeSort(right);
 
   return merge(leftSorted, rightSorted);
 
@@ -66,5 +72,47 @@ function mergeSort(numArray: number[]): number[] {
   }
 }
 
-let ans = mergeSort([1, 2, -5, 3, 20, -6])
-console.log(ans)
+// რამოდენიმე მცდელობის და შესწორებების შემდეგ, რომლებიც შეტანილ იქნა გამონაკლისი შემთხვევების დასაფარად, ფუნქციამ მიიღო შემდეგი სახე:
+// უცნაურია ის ფაქტი რომ ყოველ გაშვებაზე ვიღებ შედეგად რომ პრობლემის გადაჭრა ყველა არსებულ გადაჭრაზე სწრაფია.
+// უცნაურია რამდენადაც ვიყენებ დალაგების ალგორითმს, რომლის კომპლექსურობაც O(n logn) არის
+// ჩემ მიერ შემოთავაზებული მეორე ალგორითმი უნდა იყოს გაცილებით ოპტიმალური რესურსების თვალსაზრისით
+
+function maximumProduct(nums: number[]): number {
+  let sorted = nums.sort((a, b) => a - b);
+  let len = sorted.length;
+  let min1 = sorted[0];
+  let min2 = sorted[1];
+  let min3 = sorted[2];
+  let max1 = sorted[len - 1];
+  let max2 = sorted[len - 2];
+  let max3 = sorted[len - 3];
+
+  if (min1 >= 0 || max1 < 0) {
+    return max1 * max2 * max3;
+  }
+  if (min1 * min2 >= max2 * max3) {
+    return min1 * min2 * max1;
+  } else return max1 * max2 * max3;
+  return min1 * min2 * min3;
+}
+
+let ans = maximumProduct([
+  -710, -107, -851, 657, -14, -859, 278, -182, -749, 718, -640, 127, -930, -462,
+  694, 969, 143, 309, 904, -651, 160, 451, -159, -316, 844, -60, 611, -169, -73,
+  721, -902, 338, -20, -890, -819, -644, 107, 404, 150, -219, 459, -324, -385,
+  -118, -307, 993, 202, -147, 62, -94, -976, -329, 689, 870, 532, -686, 371,
+  -850, -186, 87, 878, 989, -822, -350, -948, -412, 161, -88, -509, 836, -207,
+  -60, 771, 516, -287, -366, -512, 509, 904, -459, 683, -563, -766, -837, -333,
+  93, 893, 303, 908, 532, -206, 990, 280, 826, -13, 115, -732, 525, -939, -787,
+]);
+let sorted = mergeSort([
+  -710, -107, -851, 657, -14, -859, 278, -182, -749, 718, -640, 127, -930, -462,
+  694, 969, 143, 309, 904, -651, 160, 451, -159, -316, 844, -60, 611, -169, -73,
+  721, -902, 338, -20, -890, -819, -644, 107, 404, 150, -219, 459, -324, -385,
+  -118, -307, 993, 202, -147, 62, -94, -976, -329, 689, 870, 532, -686, 371,
+  -850, -186, 87, 878, 989, -822, -350, -948, -412, 161, -88, -509, 836, -207,
+  -60, 771, 516, -287, -366, -512, 509, 904, -459, 683, -563, -766, -837, -333,
+  93, 893, 303, 908, 532, -206, 990, 280, 826, -13, 115, -732, 525, -939, -787,
+]);
+console.log(ans);
+console.log(sorted);
