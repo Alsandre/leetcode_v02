6f94287584210fb6baabe8f7b0192addc9dc27b5
alsandre <gabeditebuli@gmail.com>
2024-10-25 23:52:56 +0400

    იმპლემენტაცია

--- 24-09-2024__24-10-2024/25-10/682.ts
+++ 24-09-2024__24-10-2024/25-10/682.ts
@@ -10,3 +10,32 @@
 // თუ ელემენტი არის +, მაშინ დავამატოთ ახალი ქულა, რომელიც წინა ორი ქულის ჯამი იქნება
 // სხვა შემთხვევაში გვაქვს რიცხვი რომელიც შეგვყავს board ში როგორც ახალი წევრი
 // დავაბრუნოთ board მასივის ანაჯამი როგორც ფუნქციის შედეგი
+
+function calPoints(operations: string[]): number {
+  let board: number[] = [];
+  for (let i = 0; i < operations.length; i++) {
+    let el = operations[i];
+    switch (el) {
+      case "C":
+        board.pop();
+        break;
+      case "D":
+        board.push(+board[board.length - 1] * 2);
+        break;
+      case "+":
+        board.push(+board[board.length - 1] + board[board.length - 2]);
+        break;
+      default:
+        board.push(+el);
+        break;
+    }
+  }
+  return reduce(board);
+  function reduce(arr: number[]) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+}
